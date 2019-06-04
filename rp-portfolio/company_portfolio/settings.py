33f74a4c6fda691bd470a8c4e033ad5c94c7b4b6
Cassie051 <kasiaczarna051@gmail.com>
2019-06-04 09:43:53 +0200

    . .

--- rp-portfolio/company_portfolio/settings.py
+++ rp-portfolio/company_portfolio/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'company_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["company_portfolio/templates/"],
+        'DIRS': [
+            "templates/"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_URL = '/static/'
