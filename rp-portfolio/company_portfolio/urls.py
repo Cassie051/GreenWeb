06d30da553e6c0284ffb97a4594f9667e7a79319
Cassie051 <kasiaczarna051@gmail.com>
2019-06-04 09:13:44 +0200

    gggggg

--- rp-portfolio/company_portfolio/urls.py
+++ rp-portfolio/company_portfolio/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path('', include('homepage.urls')),
+    path("company/", include("company.urls")),
+    path("gallery/", include("gallery.urls")),
 ]
